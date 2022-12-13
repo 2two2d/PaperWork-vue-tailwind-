259a110398da2af603da3f02c98f7574ce9deefd
Maxon <notmax2000@mail.ru>
2022-12-13 19:11:32 +0700

    v6

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="main" class="flex flex-col items-center justify-around w-full h-[200px] mt-6">
-    <div class="w-[500px] h-[60px] bg-gray-300 rounded-full flex justify-around items-center">
-      <div class="group w-[70px] h-[50px] flex flex-col items-center hover:cursor-pointer header_button"
+    <div class="w-[500px] h-[74px] rounded-full flex justify-between items-center bg-gray-300 border-[6px] border-gray-300" id="nav">
+      <div class="group w-[135px] h-[74px] rounded-[40px] flex flex-col items-center justify-center hover:cursor-pointer relative right-2"
            v-bind:class="{active:WorldTime_is_opened}" v-on:click="open_first_component">
         <div id="img_wrapper">
-          <img src="@/assets/icons/img_time.png" alt="icon" class="w-[25px] h-[25px]">
+          <img src="@/assets/icons/img_time.png" alt="icon" class="w-[25px] h-[25px] group-hover:invert transition">
         </div>
-        <p class="group-hover:text-emerald-500">Время</p>
+        <p class="group-hover:text-white transition">Время</p>
       </div>
-      <div class="group w-[60px] h-[40px] flex flex-col items-center hover:cursor-pointer header_button"
+      <div class="group w-[135px] h-[74px] rounded-[40px] flex flex-col items-center justify-center hover:cursor-pointer"
            v-bind:class="{active:StopWatch_is_opened}" v-on:click="open_second_component">
           <div id="img_wrapper">
-            <img src="@/assets/icons/img_timer.svg" alt="icon" class="w-[25px] h-[25px]">
+            <img src="@/assets/icons/img_timer.svg" alt="icon" class="w-[25px] h-[25px] group-hover:invert transition">
           </div>
-        <p class="group-hover:text-emerald-500">Таймер</p>
+        <p class="group-hover:text-white transition">Таймер</p>
       </div>
-      <div class="group w-[60px] h-[40px] flex flex-col items-center hover:cursor-pointer header_button"
+      <div class="group w-[135px] h-[74px] rounded-[40px] flex flex-col items-center justify-center hover:cursor-pointer relative left-2"
            v-bind:class="{active:Timer_is_opened}" v-on:click="open_third_component">
         <div id="img_wrapper">
-          <img src="@/assets/icons/img_stopwatch.svg" alt="icon" class="w-[25px] h-[25px]">
+          <img src="@/assets/icons/img_stopwatch.svg" alt="icon" class="w-[25px] h-[25px] group-hover:invert transition">
         </div>
-        <p class="group-hover:text-emerald-500">Секундомер</p>
+        <p class="group-hover:text-white transition">Секундомер</p>
       </div>
     </div>
     <card v-if="WorldTime_is_opened"/>
@@ -70,13 +70,25 @@
 </script>
 
 <style scoped>
-  .active > #img_wrapper{
-    position: relative;
-    bottom: 20px;
-    background: green;
-    border-radius: 100%;
-    border: solid 5px green;
-    outline: solid 10px white;
-    transition: 0.4s;
+  p{
+    font-weight: 600;
+    color: rgb(70,70,70)
+  }
+  #nav{
+    box-shadow: rgba(255,255,255,.2) 5px 5px 5px, rgba(0,0,0,.1) -5px -5px 5px, inset rgba(255,255,255,.2) 5px 5px 5px, inset rgba(0,0,0,.1) -5px -5px 5px;
+  }
+  .active{
+    background-color: dodgerblue;
+    box-shadow: dodgerblue 0 0 6px, dodgerblue 0 0 20px;
+    transition: 0.2s;
+  }
+  .active > p{
+    color: white;
+  }
+  .active img{
+    filter: invert();
+    width: 30px;
+    height: 30px;
+    transition: 0.2s;
   }
 </style>
